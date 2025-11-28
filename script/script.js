1923ac46f869e573c108ae5ec213e19d4d3b6aee
MATHEUS DE ALMEIDA CANTARUTTI <matheus.acantarutti@senacsp.edu.br>
2025-11-27 21:56:55 -0300

    navbar

--- script/script.js
+++ script/script.js
@@ -1,23 +1,13 @@
-const hamburger=document.getElementById("hamburger");
-const nav=document.getElementById("navLinks");
-hamburger.addEventListener("click",()=>{ nav.classList.toggle("open"); });
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("navLinks");
+const icon = document.getElementById("icon");
 
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("open");
 
-// // companies
-
-// const companies = [
-//     { name: "Google", logo: "img/google.png" },
-//     { name: "Microsoft", logo: "img/microsoft.png" },
-//     { name: "Meta", logo: "img/meta.png" },
-//     { name: "Amazon", logo: "img/amazon.png" }
-//     // Adicione mais aqui sem mexer no HTML
-// ];
-
-// const grid = document.getElementById("company-grid");
-
-// companies.forEach(company => {
-//     const logo = document.createElement("img");
-//     logo.src = company.logo;
-//     logo.alt = company.name;
-//     grid.appendChild(logo);
-// });
+    if (nav.classList.contains("open")) {
+        icon.classList.replace("fa-bars", "fa-xmark");
+    } else {
+        icon.classList.replace("fa-xmark", "fa-bars");
+    }
+});
